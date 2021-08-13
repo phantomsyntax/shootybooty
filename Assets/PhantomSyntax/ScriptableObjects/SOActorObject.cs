df1139b96b9b3e4e6e079f09bbaf1097a887735a
bronsoncox <bronsoncox@hotmail.com>
2021-08-12 21:02:15 -0500

    Scriptable Objects and PlayerSpawn

---
+++ Assets/PhantomSyntax/ScriptableObjects/SOActorObject.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewActor", menuName = "Actors/New Actor Object")]
+public class SOActorObject : ScriptableObject
+{
+    public enum AttackType
+    {
+        player,
+        bullet,
+        enemyWave,
+        enemyFlee
+    }
+
+    [Header("Attack Settings")]
+    public AttackType attackType = AttackType.enemyWave;
+    public int actorHitPower = 1;
+
+    [Header("Actor Description")]
+    public string actorName = "Actor Name";
+    public string actorDescription = "Actor Description";
+
+    [Header("Actor Stats")]
+    public int actorHealth = 100;
+    public int actorSpeed = 1;
+
+    [Header("Actor Prefabs")]
+    public GameObject actorPrefab;
+    public GameObject actorBulletPrefab;
+}
---
+++ Assets/PhantomSyntax/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [Header("Player Spawn Settings")]
+    [SerializeField] private SOActorObject playerActorObject;
+    [SerializeField] private GameObject playerShipPrefab;
+    [SerializeField] private Vector3 playerSpawnPosition = Vector3.zero;
+    [SerializeField] private Quaternion playerSpawnRotation = Quaternion.Euler(0, 180, 0);
+
+    [Header("Player Initialize Settings")]
+    [SerializeField] private string playerObjectName = "Player";
+    [SerializeField] private Vector3 playerObjectScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+    private void Start()
+    {
+        NullChecks();
+
+        Spawn(playerSpawnPosition, playerSpawnRotation);
+        Initialize(playerObjectName, playerObjectScale);
+    }
+
+    private void NullChecks()
+    {
+        if (!playerShipPrefab)
+        {
+            Debug.LogWarning($"[PlayerSpawner] - playerShipPrefab has no GameObject attached!");
+        }
+        if (!playerActorObject)
+        {
+            Debug.LogWarning($"[PlayerSpawner] - playerActorObject has no SOActorObject attached!");
+        }
+    }
+
+    private void Spawn(Vector3 position, Quaternion rotation)
+    {
+        playerShipPrefab = Instantiate(playerActorObject.actorPrefab, position, rotation, transform);
+    }
+
+    private void Initialize(string name, Vector3 scale)
+    {
+        playerShipPrefab.name = name;
+        playerShipPrefab.transform.localScale = scale;
+    }
+}
